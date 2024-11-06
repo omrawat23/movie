af3e35adaa3f25ede68d11bdfa0468f20742f0eb
Om Rawat <omrawat23@gmail.com>
2024-11-07 04:35:30 +0530

    v0.1

--- src/components/VideoPage.tsx
+++ src/components/VideoPage.tsx
@@ -134,18 +134,30 @@ window.scrollTo(0, 0);
         {/* Content */}
         <div className="relative h-full max-w-7xl mx-auto px-6 py-12 flex flex-col justify-center">
           {showVideo ? (
-            <div className="aspect-video w-full max-w-4xl mx-auto bg-black rounded-xl overflow-hidden shadow-2xl">
-              <iframe
-                src={`https://vidsrc.cc/v2/embed/movie/${movieId}`}
-                // src={`https://vidbinge.dev/embed/movie/${movieId}`}
-                className="aspect-video w-full h-auto rounded-lg"
-                width="1280"
-                height="720"
-                title="Video player"
-                frameBorder="0"
-                allowFullScreen
-                allowTransparency
-              ></iframe>
+            <div  onClick={(e) => e.stopPropagation()} className="aspect-video w-full max-w-4xl mx-auto bg-black rounded-xl overflow-hidden shadow-2xl">
+             {/* <iframe
+  src={showVideo ? `https://vidsrc.cc/v2/embed/movie/${movieId}` : "about:blank"}
+  className="aspect-video w-full h-auto rounded-lg relative"
+  width="1280"
+  height="720"
+  title="Video player"
+  frameBorder="0"
+  allowFullScreen
+  allowTransparency
+  sandbox="allow-same-origin allow-scripts" // Limits the iframe's capabilities
+></iframe> */}
+<iframe
+  src={showVideo ? `https://embed.su/embed/movie/${movieId}` : "about:blank"}
+  className="aspect-video w-full h-auto rounded-lg relative"
+  width="1280"
+  height="720"
+  title="Video player"
+  frameBorder="0"
+  allowFullScreen
+  allowTransparency
+  
+></iframe>
+
             </div>
           ) : (
             <>
