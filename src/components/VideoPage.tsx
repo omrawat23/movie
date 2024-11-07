21b8611fc5a54de8828b8a68831fcefd268de772
Om Rawat <omrawat23@gmail.com>
2024-11-07 06:02:13 +0530

    v78

--- src/components/VideoPage.tsx
+++ src/components/VideoPage.tsx
@@ -136,18 +136,17 @@ window.scrollTo(0, 0);
           {showVideo ? (
             <div  onClick={(e) => e.stopPropagation()} className="aspect-video w-full max-w-4xl mx-auto bg-black rounded-xl overflow-hidden shadow-2xl">
              <iframe
-  src={`https://embed.su/embed/movie/${movieId}`}
+  src={`https://vidsrc.cc/v2/embed/movie/${movieId}`}
   className="aspect-video w-full h-auto rounded-lg relative"
   width="1280"
   height="720"
   title="Video player"
   frameBorder="0"
   allowFullScreen
-  allowTransparency
   sandbox="allow-same-origin allow-scripts" // Limits the iframe's capabilities
 ></iframe>
 {/* <iframe
-   src={showVideo ? `http://localhost:5000/proxy/video/${movieId}` : "about:blank"}
+   src={showVideo ? `https://embed.su/embed/movie/${movieId}` : "about:blank"}
   className="aspect-video w-full h-auto rounded-lg relative"
   width="1280"
   height="720"
